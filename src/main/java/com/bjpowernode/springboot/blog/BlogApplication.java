2b8ec759a8ec2e5122cc9bf5d87f1ee2fda60a7a
EzraChai <juanzhe2@gmail.com>
2021-03-04 19:36:26 +0800

    add-db

--- src/main/java/com/bjpowernode/springboot/blog/BlogApplication.java
+++ src/main/java/com/bjpowernode/springboot/blog/BlogApplication.java
@@ -2,11 +2,12 @@ package com.bjpowernode.springboot.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class BlogApplication {
+public class BlogApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
